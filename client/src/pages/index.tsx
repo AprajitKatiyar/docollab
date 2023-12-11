3e8d1fac68c3093d4ec43ca967263fea8655bf3d
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2023-12-12 02:47:48 +0530

    Adding landing page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,41 +1,53 @@
-import { Inter } from "next/font/google";
-import { signIn, useSession, signOut } from "next-auth/react";
-
+import React, { useCallback, useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 export default function Home() {
-  const session = useSession();
-  console.log(session);
-  console.error(session);
-  // const handleOnClick = async () => {
-  //   const res = await signIn("credentials", {
-  //     email: "john@gmail.com",
-  //     password: "1234",
-  //     redirect: true,
-  //   });
-  //   console.log(res);
-  // };
-
   return (
-    <div style={{ height: 60, background: "white", padding: 10 }}>
-      {session.data && (
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <h3 color="black">{session.data.user?.email}</h3>
-          <div>
-            <button color="black" onClick={() => signOut()}>
-              Logout
-            </button>
-          </div>
+    <div className="h-screen w-full relative">
+      <div className="absolute h-full w-full grid grid-cols-2">
+        <div className="bg-gray-100" />
+        <div className="relative">
+          <h1 className=" select-none ml-[-10px] text-9xl font-extrabold text-gray-100">
+            Do.
+          </h1>
+          <h1 className="select-none ml-[-10px] text-9xl font-extrabold text-gray-100">
+            Collab.
+          </h1>
         </div>
-      )}
-      {!session.data && (
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <h3 color="black">Docollab</h3>
-          <div>
-            <button color="black" onClick={() => signIn()}>
-              Sign In
-            </button>
-          </div>
+      </div>
+      <div className="absolute m-auto w-full top-[300px] flex justify-center">
+        <h1 className="select-none text-6xl font-extrabold mr-2i">Where</h1>
+        <h1 className="select-none text-6xl font-extrabold mr-2 bg-clip-text bg-[#8F48EB] text-transparent">
+          Docs
+        </h1>
+        <h1 className="select-none text-6xl font-extrabold mr-2">Meet</h1>
+        <h1 className="select-none text-6xl font-extrabold  bg-clip-text bg-[#8F48EB] text-transparent">
+          Doodles
+        </h1>
+        <h1 className="select-none text-6xl font-extrabold mr-2">. </h1>
+      </div>
+      <div className="absolute m-auto w-full top-[500px] flex justify-center">
+        <button className="border border-gray-400 rounded-full p-4">
+          <FaArrowRight size="40" strokeWidth="2" color="#8F48EB" />
+        </button>
+      </div>
+      <div className="absolute bottom-56 w-60 h-52 bg-white">
+        <div className="relative w-60 left-36 top-10">
+          <h1 className="text-2xl font-bold">Seamless Integration</h1>
+          <p className="py-2 text-gray-600">
+            Combine the power of realtime collaborative document editing and
+            flow creation all in one platform.
+          </p>
         </div>
-      )}
+      </div>
+      <div className="absolute bottom-56 right-0 w-60 h-52 bg-gray-100">
+        <div className="relative w-60 right-36 top-10">
+          <h1 className="text-2xl font-bold">Interactive Widgets</h1>
+          <p className="py-2 text-gray-600">
+            Combine the power of realtime collaborative document editing and
+            flow creation all in one platform.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
