ed5af44ef03d2c2995433b1d1eb1a3780b10e8c8
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2024-01-02 23:51:28 +0530

    Adding appbar

---
+++ client/src/components/AppBar.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import { MdOutlineSubject } from "react-icons/md";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+export default function AppBar({ session }: any) {
+    console.log("aa",session);
+  return (
+    <div className="flex justify-between p-4">
+      <div className="flex justify-start items-center">
+        <MdOutlineSubject size="25" color="#8F48EB" />
+        <h1 className="text-xl font-extrabold ml-2 select-none">Do.Collab.</h1>
+      </div>
+      <div className="flex justify-end">
+        {session && (
+          <button className="flex justify-center items-center border-collapse">
+            <h1 className="font-extrabold">Hi</h1>
+            <h1 className="font-extrabold bg-clip-text bg-[#8F48EB] text-transparent mr-1">
+              ,{" "}
+            </h1>
+            <h1 className="font-extrabold">Aprajit</h1>
+            <IoMdArrowDropdown size="25" color="#8F48EB" />
+          </button>
+        )}
+        {!session && (
+          <div className="flex">
+            <button className="mr-3 border-collapse text-lg font-bold">
+              Login
+            </button>
+            <button className="border-collapse text-lg font-bold  text-[#8F48EB]">
+              SignUp
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
