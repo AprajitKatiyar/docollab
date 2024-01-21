b13f95080a7293f9dee8b2c4c6bcc503787f3ed5
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2024-01-21 20:45:54 +0530

    Api for saving and fetching doc and flow

--- server/src/index.ts
+++ server/src/index.ts
@@ -182,6 +182,92 @@ app.post("/projects/createFlow", async (req, res) => {
     });
   }
 });
+app.put("/docs/save/:docId", async (req, res) => {
+  try {
+    const { docId } = req.params;
+    const { data } = req.body;
+    const updatedDoc = await prisma.doc.update({
+      where: { id: docId },
+      data: {
+        data: data,
+      },
+    });
+    if (updatedDoc)
+      res.json({
+        message: "Doc saved successfully",
+        updatedDoc: updatedDoc,
+      });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+app.get("/docs/getDoc/:docId", async (req, res) => {
+  try {
+    const { docId } = req.params;
+    const doc = await prisma.doc.findUnique({
+      where: { id: docId },
+    });
+    if (doc)
+      res.json({
+        message: "Doc found successfully",
+        doc: doc,
+      });
+    else {
+      res.status(403).json({ message: "Invalid Doc id" });
+    }
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+app.put("/flows/save/:flowId", async (req, res) => {
+  try {
+    const { flowId } = req.params;
+    const { data } = req.body;
+    const updatedFlow = await prisma.flow.update({
+      where: { id: flowId },
+      data: {
+        data: data,
+      },
+    });
+    if (updatedFlow)
+      res.json({
+        message: "Flow saved successfully",
+        updatedFlow: updatedFlow,
+      });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+app.get("/flows/getFlow/:flowId", async (req, res) => {
+  try {
+    const { flowId } = req.params;
+    const flow = await prisma.flow.findUnique({
+      where: { id: flowId },
+    });
+    if (flow)
+      res.json({
+        message: "Flow found successfully",
+        flow: flow,
+      });
+    else {
+      res.status(403).json({ message: "Invalid Flow id" });
+    }
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
 
 httpServer.listen(port, () => {
   console.log(`Backend server is up and running at http://localhost:${port}`);
