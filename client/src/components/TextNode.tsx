ee9b4cf010a8950e429aac1e72b028ad7bdd5549
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2024-02-11 23:52:10 +0530

    Custom handles

--- client/src/components/TextNode.tsx
+++ client/src/components/TextNode.tsx
@@ -12,8 +12,24 @@ function TextNode({ data, id, updateLabel }: any) {
     setValue(data.label);
   }, [data]);
   return (
-    <div className="flex justify-center content-center shadow-md rounded-md border-2">
-      <input type="text" value={value} onChange={handleInputChange} />
+    <div className="flex justify-center items-center shadow-md rounded-md border-2 w-52 h-16 bg-white">
+      <input
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        className="w-24 h-8 text-center border-collapse focus:outline-none"
+      />
+
+      <Handle
+        type="source"
+        position={Position.Left}
+        className="w-16 !bg-teal-500"
+      />
+      <Handle
+        type="target"
+        position={Position.Right}
+        className="w-16 !bg-teal-500"
+      />
     </div>
   );
 }
